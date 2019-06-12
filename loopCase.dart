5c02a1570ffa8976d7427fcf7c08f1dbffcac122
Eduardo Uliana <edu02_rko@hotmail.com>
2019-06-12 00:12:23 -0300

    Adicionado exemplo de uso do switch case

---
+++ loopCase.dart
@@ -0,0 +1,17 @@
+void main() {
+  String meuNome = "Eduardo";
+  
+  switch(meuNome) {
+    case "Eduardo":
+      print("Eduardo");
+      break;
+    case "Antônio":
+      print("Antônio");
+      break;
+    case "Uliana":
+      print("Uliana");
+      break;
+    default:
+      print("Sem nome");
+  }
+}

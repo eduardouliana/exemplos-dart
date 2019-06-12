a798f4451a44babdb9fbdaa33e86e6c2f7b8de71
Eduardo Uliana <edu02_rko@hotmail.com>
2019-06-12 00:11:51 -0300

    Adicionado exemplo de de calculo de média

---
+++ testeMedia.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Teste média
+  
+  double nota = 7.0;
+  
+  if (nota < 5.0){
+    print("Exame");
+  } else if (nota == 10.0){
+    print("Parabéns, tirou 10 uhu");
+  } else {
+    print("Aprovado");
+  }
+}

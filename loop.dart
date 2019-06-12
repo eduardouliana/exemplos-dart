a979c88014f1b7fcfbeff7a41c54126f908d5383
Eduardo Uliana <edu02_rko@hotmail.com>
2019-06-12 00:19:42 -0300

    Adicionado exemplo de uso de loops

---
+++ loop.dart
@@ -0,0 +1,18 @@
+void main() {
+	
+  for (int i = 1; i<=10; i++){
+    print(i);
+  }
+  
+  int j = 1;
+  while (j <= 10) {
+    print(j);
+    j++;
+  }
+  
+  int k = 1;
+  do {
+    print(k);
+    k++;
+  } while (k <=10);
+}

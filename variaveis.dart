e32264c64cb41b460f421b7ef5ab032aa43ab743
Eduardo Uliana <edu02_rko@hotmail.com>
2019-06-12 00:07:28 -0300

    Adicionado exemplo de variáveis

---
+++ variaveis.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nome = "Eduardo Uliana";
+  int idade = 24;
+  double altura = 1.83;
+  bool vivo = true;
+  var tipoDefinido = "Primeiro valor que receber, seta o tipo da variável";
+  dynamic tipoIndefinido = "Pode atribuir qualquer coisa para esta variável";
+  
+  print("Seu nome é: $nome");
+  print("Sua altura é: $altura");
+  print("Sua idade é: $idade");
+  print("Estou vivo?: $vivo");
+}
